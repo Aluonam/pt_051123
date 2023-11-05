1695b3aeeb9c6121b547d4333c94636057c07379
Paula Cano <paulacanoroman@gmail.com>
2023-11-05 20:22:14 +0100

    esqueleto input y botón

--- pages/components/DeleteLastWord.jsx
+++ pages/components/DeleteLastWord.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
 const DeleteLastWord = () => {
+
+
   return (
+    <>
     <div>DeleteLastWord</div>
+    <input onChange={(e)=>{console.log(e.target.value)}}></input>
+    <br></br>
+    <button>Borra la última palabra</button>
+    </>
   )
 }
 
